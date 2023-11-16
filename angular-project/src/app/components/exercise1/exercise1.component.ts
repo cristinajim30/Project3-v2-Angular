5b8963f10eb3ee709e98b9ac3fb72e0e876c25dd
Cristina <c>
2023-11-16 13:20:16 +0100

    content of components

--- angular-project/src/app/components/exercise1/exercise1.component.ts
+++ angular-project/src/app/components/exercise1/exercise1.component.ts
@@ -6,5 +6,54 @@ import { Component } from '@angular/core';
   styleUrl: './exercise1.component.css'
 })
 export class Exercise1Component {
+  userName: string = ''.toUpperCase();
+  fontList: string[] = ['Poppins','Roboto Mono', 'Dancing Script', 'Josefin Sans'];
+  aligmentList: string[] = ['Left','Center', 'Right'];
+ 
 
+  optionSelected: string = this.fontList[0]; 
+  fontSelected: string = this.fontList[0];
+  numberSize: any = 60; //size by default
+  inputSize: string = ''; // Recovering the input value
+  cbAligment: string = this.aligmentList[1]; //check this radio by default
+  aligmentSelected: string = this.aligmentList[1]; //aligment by default
+
+
+  selectStyle(): void {
+    this.fontSelected = this.optionSelected;
+
+  }
+
+
+  checkNumber(event: KeyboardEvent): any {
+
+    // Allow backward key
+    if (event.key === 'Backspace') {
+      return;
+    }
+    // Recovering the current input value
+    const inputValue = (event.target as HTMLInputElement).value;
+
+    // Check if value is a number from 0 to 300
+    const isValidNumber = /^\d+$/.test(inputValue) && +inputValue >= 0 && +inputValue <= 300;
+
+    if (!isValidNumber) {
+      alert('Ingrese un número válido del 0 al 300');
+      this.inputSize = '';
+
+    } else{ //if number is valid
+      console.log("es numero valido");
+      this.changeSize(); //call function to change size
+    }
+
+  }
+
+  
+  changeSize(): void {
+    this.numberSize = this.inputSize;
+  }
+
+  selectAligment(): void {
+    this.aligmentSelected = this.cbAligment;
+  }
 }
